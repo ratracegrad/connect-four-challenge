5537cec32de22d1331586a334a47df4d6e61d84d
Jennifer Bland <ratracegrad@gmail.com>
2017-05-18 19:27:15 -0400

    Added font awesome and then added icons to footer

--- client/app/components/footer/footer.js
+++ client/app/components/footer/footer.js
@@ -1,8 +1,14 @@
 angular.module('app')
   .component('footer',
     {
-      template: `<div class="flexContainer footer">
-             <span>Jennifer Bland</span>
-           </div>`
+      template:
+        `<div class="flexContainer footer">
+             <span>&copy; &nbsp;Jennifer Bland</span>
+             <a href="https://github.com/ratracegrad" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
+             <a href="https://www.linkedin.com/in/ratracegrad/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+             <a href="http://www.jenniferbland.com" target="_blank"><i class="fa fa-globe" aria-hidden="true"></i></a>
+             <span class="sourceCode" target="_blank"><a href="https://github.com/ratracegrad/connect-four-challenge">Source Code</a></span>
+        </div>
+        `
   });
 
